3f551c1ddbbd9865ac71a9fce07f015a58dff960
Daninzrth <dani.angulo1910@gmail.com>
2022-04-21 19:32:36 +0000

    Botones y funcionalidad

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,8 +5,28 @@ const Home = () => {
 	const [iluminado, setIluminado] = useState(false);
 	const [iluminado1, setIluminado1] = useState(false);
 	const [iluminado2, setIluminado2] = useState(false);
+	const [iluminado3, setIluminado3] = useState(false);
+	const [outpurple, setPurple] = useState(true);
+	let iluminados = [iluminado, iluminado1, iluminado2];
+	function changeIluminado() {
+		var random = Math.floor(Math.random() * 3);
+		if (iluminados[random]) {
+			changeIluminado();
+		} else {
+			setIluminado(false);
+			setIluminado1(false);
+			setIluminado2(false);
+			if (random == 0) {
+				setIluminado(true);
+			} else if (random == 1) {
+				setIluminado1(true);
+			} else if (random == 2) {
+				setIluminado2(true);
+			}
+		}
+	}
 
-	return (
+	return outpurple ? (
 		<div className="container py-3">
 			<div
 				className={iluminado ? "select " : "no"}
@@ -32,6 +52,55 @@ const Home = () => {
 					setIluminado(false) &
 					setIluminado1(false)
 				}></div>
+			<div className="row my-5">
+				<button onClick={changeIluminado}> Change! </button>
+				<button onClick={() => setPurple(false)}>
+					{" "}
+					Add light Purple!{" "}
+				</button>
+			</div>
+		</div>
+	) : (
+		<div className="container py-3" style={{ height: "440px" }}>
+			<div
+				className={iluminado ? "select " : "no"}
+				id="redlight"
+				onClick={() =>
+					setIluminado(true) &
+					setIluminado1(false) &
+					setIluminado2(false) &
+					setIluminado3(false)
+				}></div>
+			<div
+				className={iluminado1 ? "select" : "no"}
+				id="yellowlight"
+				onClick={() =>
+					setIluminado1(true) &
+					setIluminado(false) &
+					setIluminado2(false) &
+					setIluminado3(false)
+				}></div>
+			<div
+				className={iluminado2 ? "select" : "no"}
+				id="greenlight"
+				onClick={() =>
+					setIluminado2(true) &
+					setIluminado(false) &
+					setIluminado1(false) &
+					setIluminado3(false)
+				}></div>
+			<div
+				className={iluminado3 ? "select" : "no"}
+				id="purplelight"
+				onClick={() =>
+					setIluminado3(true) &
+					setIluminado(false) &
+					setIluminado1(false) &
+					setIluminado2(false)
+				}></div>
+			<div className="row my-5">
+				<button onClick={() => setPurple(true)}> Change! </button>
+			</div>
 		</div>
 	);
 };
