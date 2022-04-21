a514eb5822f93ea9f5572d87055ae71143ff1e1e
Daninzrth <dani.angulo1910@gmail.com>
2022-04-21 18:23:34 +0000

    Listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,37 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [iluminado, setIluminado] = useState(false);
+	const [iluminado1, setIluminado1] = useState(false);
+	const [iluminado2, setIluminado2] = useState(false);
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container py-3">
+			<div
+				className={iluminado ? "select " : "no"}
+				id="redlight"
+				onClick={() =>
+					setIluminado(true) &
+					setIluminado1(false) &
+					setIluminado2(false)
+				}></div>
+			<div
+				className={iluminado1 ? "select" : "no"}
+				id="yellowlight"
+				onClick={() =>
+					setIluminado1(true) &
+					setIluminado(false) &
+					setIluminado2(false)
+				}></div>
+			<div
+				className={iluminado2 ? "select" : "no"}
+				id="greenlight"
+				onClick={() =>
+					setIluminado2(true) &
+					setIluminado(false) &
+					setIluminado1(false)
+				}></div>
 		</div>
 	);
 };
